cfc0089510f9d85ecb19e4ad9c49b85d79a8d2d9
MiyeonSana <136689210+MiyeonSana@users.noreply.github.com>
2023-11-04 20:40:03 +0800

    Update Food.java

--- Food.java
+++ Food.java
@@ -4,10 +4,19 @@
  */
 package mm;
 
-/**
- *
- * @author pc
- */
+
 public class Food {
-    
+    private int id; // this is id from database
+    private String foodName; // from database
+    private double price; // from database
+
+    public Food(int id, String foodName, double price){
+        this.id = id;
+        this.foodName = foodName;
+        this.price = price;
+    }
+
+    public int getId(){ return id; }
+    public String getFoodName() { return foodName; }
+    public double getPrice() { return price;}
 }
